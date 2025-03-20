ea0413bd73723b740b9755f8d57cacaa8396cddb
zsh003 <2138155993@qq.com>
2025-03-20 11:33:23 +0800

    gitignore

--- target/classes/crane.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `crane` (`num`, `type`, `weight`, `sit`) VALUES (1, '起升制动器', 953.5, 0);
-INSERT INTO `crane` (`num`, `type`, `weight`, `sit`) VALUES (2, '起升制动器', 953.5, 0);
-INSERT INTO `crane` (`num`, `type`, `weight`, `sit`) VALUES (3, '起升制动器', 953.5, 1);
-INSERT INTO `crane` (`num`, `type`, `weight`, `sit`) VALUES (4, '起升制动器', 953.5, 1);
